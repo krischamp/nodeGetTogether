39ca0e3347787b2d80eb70d9f58ae1b100b9cea9
Kris Champ <krischamp@gmail.com>
2016-04-13 15:20:18 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+/*
+*	server.js
+*/
+
+
+const port = process.env.PORT || '8080';
+const host = process.env.HOST || '0.0.0.0';
+const express = require('express');
+const app = express();
+
+//app.use(require('./app'));
+
+app.listen(port, host); // port always first
+
+console.log('Server running %s:%d...', host, port);
+
+module.exports = app;
